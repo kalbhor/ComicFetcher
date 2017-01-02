e8248796d7b0a564a6ef1b7df28861bddbd11252
Lakshay Kalbhor <lakshay@Lakshays-MacBook-Pro.local>
2017-01-02 13:58:52 +0530

    Comics

--- ComicFetcher/command_line.py
+++ ComicFetcher/command_line.py
@@ -21,6 +21,7 @@ def main():
 	parser = argparse.ArgumentParser(description="Download various comics")
 	parser.add_argument('--cyanide', action='store_true', help='Download latest cyanide and happiness comic')
 	parser.add_argument('--xkcd', action='store_true', help='Download latest xkcd comic')
+	parser.add_argument('--channelate', action='store_true', help='Download latest channelate comic')
 
 	args = parser.parse_args()
 
@@ -29,6 +30,9 @@ def main():
 
 	if args.xkcd:
 		xkcd()
+
+	if args.channelate:
+		channelate()
 
 
 def explosm():
@@ -47,8 +51,22 @@ def xkcd():
 	response = requests.get('http://xkcd.com/info.0.json').json()
 	comic_url = response['img']
 	file_name = 'xkcd {} {}'.format(DATE, response['title'])
+	print(comic_url)
 	urlretrieve(comic_url, file_name + FILE_TYPE)
 	print('Saved %s'%file_name)
+
+def channelate():
+
+	response = requests.get('http://www.channelate.com/')
+	print(response.status_code)
+	soup = BeautifulSoup(response, 'html.parser')
+	comic_tag = soup.find('div', {'class':'comicpane'}).find('img')
+	comic_url = "{https}{url}".format(https='http:', url=comic_tag.get('src'))
+	file_name = "Channelate {} {comic_name}".format(DATE,comic_name=comic_tag.get('title'))
+	print(comic_url)
+	urlretrieve(comic_url, "lol.png")
+	print('Saved %s'%(file_name))
+
 
 
 if __name__ == '__main__':
